445152a6087a3a5cb65d9b320558f1f7a142af6e
Will Lehman <william.p.lehman@usace.army.mil>
2021-08-03 18:57:49 -0700

    writing out damages instead of quantiles.

--- compute/eadestimate.go
+++ compute/eadestimate.go
@@ -6,23 +6,23 @@ import (
 	"math/rand"
 
 	"github.com/HenryGeorgist/go-fathom/hazard_providers"
-	"github.com/HydrologicEngineeringCenter/go-statistics/data"
 	"github.com/USACE/go-consequences/compute"
 	"github.com/USACE/go-consequences/hazards"
 	"github.com/USACE/go-consequences/structures"
 )
 
-func ComputeEadDistribution(sfc hazard_providers.StageFrequencyCurve, s structures.StructureStochastic, binWidth float64, binStart float64, binEnd float64, iterations int) (*data.InlineHistogram, error) {
-	eaddist := data.Init(binWidth, binStart, binEnd) //percent of total value?
-
-	structureSeed := 1234                                                     //create a seed sequence for the structure
-	stageFrequencySeed := 4431                                                //create a seed sequence for the stage frequency
-	structureRand := rand.New(rand.NewSource(int64(structureSeed)))           //not concurrent safe
-	stageFrequencyRand := rand.New(rand.NewSource(int64(stageFrequencySeed))) //not concurrent safe.
+func ComputeEadDistribution(sfc hazard_providers.StageFrequencyCurve, s structures.StructureStochastic, binWidth float64, binStart float64, binEnd float64, iterations int) ([]float64, error) {
+	//eaddist := data.Init(binWidth, binStart, binEnd) //percent of total value?
+	eadlist := make([]float64, iterations)
+	structureSeed := 1234 //create a seed sequence for the structure
+	//stageFrequencySeed := 4431                                                //create a seed sequence for the stage frequency
+	structureRand := rand.New(rand.NewSource(int64(structureSeed))) //not concurrent safe
+	//stageFrequencyRand := rand.New(rand.NewSource(int64(stageFrequencySeed))) //not concurrent safe.
 	//for some number of iterations
 	for i := 0; i < iterations; i++ {
 		ds := s.SampleStructure(structureRand.Int63()) // sample a structure
-		dsfc := sfc.Sample(stageFrequencyRand.Float64())
+		//dsfc := sfc.Sample(stageFrequencyRand.Float64())
+		dsfc := sfc.Sample(float64(float64(i)+0.5) / float64(iterations))
 		realizationDamages := make([]float64, len(dsfc))
 		//for each deterministic ordinate compute damage for the sampled structure
 		for idx, d := range dsfc {
@@ -50,12 +50,12 @@ func ComputeEadDistribution(sfc hazard_providers.StageFrequencyCurve, s structur
 			sdam := stdam.(float64)
 			cdam := condam.(float64)
 			tdam := sdam + cdam
-			totval := ds.StructVal + ds.ContVal
+			/*totval := ds.StructVal + ds.ContVal
 			dampercent := tdam / (totval)
 			if totval == 0 {
 				dampercent = 0
-			}
-			realizationDamages[idx] = dampercent
+			}*/
+			realizationDamages[idx] = tdam
 
 		}
 		eadEst := compute.ComputeSpecialEAD(realizationDamages, sfc.Frequencies)
@@ -68,7 +68,8 @@ func ComputeEadDistribution(sfc hazard_providers.StageFrequencyCurve, s structur
 		if eadEst > 1 {
 			fmt.Println(fmt.Sprintf("%v", eadEst))
 		}
-		eaddist.AddObservation(eadEst)
+		//eaddist.AddObservation(eadEst)
+		eadlist[i] = eadEst
 	}
-	return eaddist, nil
+	return eadlist, nil
 }
--- compute/fathom_ead_simulation.go
+++ compute/fathom_ead_simulation.go
@@ -5,27 +5,26 @@ import (
 	"os"
 
 	"github.com/HenryGeorgist/go-fathom/hazard_providers"
-	"github.com/HydrologicEngineeringCenter/go-statistics/data"
 	"github.com/USACE/go-consequences/consequences"
 	"github.com/USACE/go-consequences/structures"
 )
 
 func ComputeEadByFips(ds hazard_providers.StochasticDataSet, sp consequences.StreamProvider, fips string, outputFile *os.File, newData bool) bool {
 	fmt.Println("Computing by fips for " + fips)
-	binWidth := .05
+	binWidth := .01
 	binStart := 0.0
 	binEnd := 1.0
-	iterations := 100
+	iterations := 20
 	stringifiedHeaders := make([]string, 4)
-	stringifiedHeaders[0] = writeHistoHeader("fluv", 2020, binWidth, binStart, binEnd)
-	stringifiedHeaders[1] = writeHistoHeader("fluv", 2050, binWidth, binStart, binEnd)
-	stringifiedHeaders[2] = writeHistoHeader("cstl", 2020, binWidth, binStart, binEnd)
-	stringifiedHeaders[3] = writeHistoHeader("cstl", 2050, binWidth, binStart, binEnd)
-	outputFile.WriteString(fmt.Sprintf("FD_ID,X,Y,County,CB,OccType,DamCat,foundHt,StructVal,ContVal,PopDay,PopNight,fluv_2020_Mean_EAD,cstl_2020_Mean_EAD,fluv_2050_Mean_EAD,cstl_2050_Mean_EAD%s%s%s%s\n", stringifiedHeaders[0], stringifiedHeaders[2], stringifiedHeaders[1], stringifiedHeaders[3]))
+	stringifiedHeaders[0] = writeHistoHeader("fluv", 2020, iterations)
+	stringifiedHeaders[1] = writeHistoHeader("fluv", 2050, iterations)
+	stringifiedHeaders[2] = writeHistoHeader("cstl", 2020, iterations)
+	stringifiedHeaders[3] = writeHistoHeader("cstl", 2050, iterations)
+	outputFile.WriteString(fmt.Sprintf("FD_ID,X,Y,County,CB,OccType,DamCat,foundHt,StructVal,ContVal,PopDay,PopNight%s%s%s%s\n", stringifiedHeaders[0], stringifiedHeaders[2], stringifiedHeaders[1], stringifiedHeaders[3]))
 	years := [2]int{2020, 2050}
 	fluvial := [2]bool{true, false}
-	histograms := make([]*data.InlineHistogram, 4)
-	histogramExists := make([]bool, 4)
+	eads := make([][]float64, 4)
+	eadsExists := make([]bool, 4)
 	index := 0
 	//otfdm := FoundationDistributionMap()
 	sp.ByFips(fips, func(cr consequences.Receptor) {
@@ -43,36 +42,36 @@ func ComputeEadByFips(ds hazard_providers.StochasticDataSet, sp consequences.Str
 						sfc, err := ds.ProvideStageFrequencyCurve(str.Name, y, flu)
 						if err == nil {
 							ih, err := ComputeEadDistribution(sfc, str, binWidth, binStart, binEnd, iterations)
-							histograms[index] = ih
+							eads[index] = ih
 							if err != nil {
-								histogramExists[index] = false
+								eadsExists[index] = false
 							} else {
-								histogramExists[index] = true
+								eadsExists[index] = true
 							}
 						} else {
-							histogramExists[index] = false
+							eadsExists[index] = false
 						}
 						index += 1
 					}
 				}
 				//write to output file.
-				results := make([]float64, 4)
+				//results := make([]float64, 4)
 				stringifiedhistos := make([]string, 4)
 				eadsum := 0.0
-				for idx, b := range histogramExists {
+				for idx, b := range eadsExists {
 					if b {
-						results[idx] = histograms[idx].Mean() * (str.ContVal.CentralTendency() + str.StructVal.CentralTendency())
-						eadsum += results[idx]
-						stringifiedhistos[idx] = writeHistoValues(histograms[idx], iterations, str, binWidth, binStart, binEnd)
+						//results[idx] = eads[idx].Mean() * (str.ContVal.CentralTendency() + str.StructVal.CentralTendency())
+						//eadsum += results[idx]
+						stringifiedhistos[idx] = writeHistoValues(eads[idx])
 					} else {
-						results[idx] = 0
-						stringifiedhistos[idx] = writeNilHistoValues(histograms[idx], iterations, str, binWidth, binStart, binEnd)
+						//results[idx] = 0
+						stringifiedhistos[idx] = writeNilHistoValues(eads[idx], iterations)
 					}
 				}
 				//outputFile.WriteString("FD_ID,X,Y,County,CB,OccType,DamCat,foundHt,StructVal,ContVal,PopDay,PopNight,fluv_2020_EAD,cstl_2020_EAD,fluv_2050_EAD,cstl_2050_EAD\n")
 				county := str.CBFips[0:5] //county is first five characters of the cb.
 				if eadsum > 0 {
-					outputFile.WriteString(fmt.Sprintf("%s,%f,%f,%s,%s,%s,%s,%f,%f,%f,%d,%d,%f,%f,%f,%f%s%s%s%s\n", str.Name, str.X, str.Y, county, str.CBFips, str.OccType.Name, str.DamCat, str.FoundHt.CentralTendency(), str.StructVal.CentralTendency(), str.ContVal.CentralTendency(), str.Pop2amu65+str.Pop2amo65, str.Pop2pmu65+str.Pop2pmo65, results[0], results[2], results[1], results[3], stringifiedhistos[0], stringifiedhistos[2], stringifiedhistos[1], stringifiedhistos[3]))
+					outputFile.WriteString(fmt.Sprintf("%s,%f,%f,%s,%s,%s,%s,%f,%f,%f,%d,%d%s%s%s%s\n", str.Name, str.X, str.Y, county, str.CBFips, str.OccType.Name, str.DamCat, str.FoundHt.CentralTendency(), str.StructVal.CentralTendency(), str.ContVal.CentralTendency(), str.Pop2amu65+str.Pop2amo65, str.Pop2pmu65+str.Pop2pmo65, stringifiedhistos[0], stringifiedhistos[2], stringifiedhistos[1], stringifiedhistos[3]))
 				}
 			}
 		}
@@ -81,34 +80,24 @@ func ComputeEadByFips(ds hazard_providers.StochasticDataSet, sp consequences.Str
 	fmt.Println("Completed Computing by fips " + fips)
 	return true
 }
-func writeHistoHeader(fluv string, year int, binWidth float64, binStart float64, binEnd float64) string {
-	loc := binStart + binWidth
-	binEndEpsilon := binEnd + (binWidth * .5)
+func writeHistoHeader(fluv string, year int, iterations int) string {
 	ret := ""
-	for loc <= binEndEpsilon {
-		ret += fmt.Sprintf(",%s_%v_%f", fluv, year, loc)
-		loc += binWidth
+	for i := 0; i < iterations; i++ {
+		ret += fmt.Sprintf(",%s_%v_%v", fluv, year, i+1)
 	}
 	return ret
 }
-func writeHistoValues(ih *data.InlineHistogram, iterations int, s structures.StructureStochastic, binWidth float64, binStart float64, binEnd float64) string {
+func writeHistoValues(ih []float64) string {
 	ret := ""
-	totval := s.StructVal.CentralTendency() + s.ContVal.CentralTendency()
-	loc := binStart + binWidth
-	binEndEpsilon := binEnd + (binWidth * .5)
-	for loc <= binEndEpsilon {
-		ret += fmt.Sprintf(",%f", totval*ih.InvCDF(loc))
-		loc += binWidth
+	for _, d := range ih {
+		ret += fmt.Sprintf(",%f", d)
 	}
 	return ret
 }
-func writeNilHistoValues(ih *data.InlineHistogram, iterations int, s structures.StructureStochastic, binWidth float64, binStart float64, binEnd float64) string {
+func writeNilHistoValues(ih []float64, iterations int) string {
 	ret := ""
-	loc := binStart + binWidth
-	binEndEpsilon := binEnd + (binWidth * .5)
-	for loc <= binEndEpsilon {
+	for i := 0; i < iterations; i++ {
 		ret += fmt.Sprintf(",%f", 0.0)
-		loc += binWidth
 	}
 	return ret
 }
